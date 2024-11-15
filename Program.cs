7422fcb747c2d23dc13706916e7ff5709fa331a8
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2024-11-15 12:07:33 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,142 +1,153 @@
-﻿namespace Taschenrechner
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Test
 {
     internal class Program
     {
+        private static readonly object fileLock = new object(); // Lock-Objekt
+
         static void Main(string[] args)
         {
-            int rechnen = 1;
-            double zahl1;
-            double zahl2;
-            double zahl3;
-            double result = 0;
-            string op;
-            bool end = true;
-            bool y = true;
+            int r;
+            int d;
+            int v;
+            int h;
+            int u;
+            int f;
+            int o;
 
-            Console.WriteLine("Taschenrechner");
+            double wert1 = 0;
+            double wert2 = 0;
+            double wert3 = 0;
+            string eingabe1;
+            string eingabe2;
+            string eingabeResult;
+            int result;
 
-            while (end)
+
+            Console.WriteLine("Welchen Wert willst du errechnen? ");
+            eingabeResult = Console.ReadLine();            
+
+            switch (eingabeResult)
             {
-                y = true;
+                case "r":
+                    
+                    break;
+                case "d":
 
-                Console.Write("Gib eine Zahl ein: ");
-                zahl1 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case "v":
+                    Console.WriteLine("Erforderlicher Wert: h\nMögliche Werte: U,d,r,A");
+                    Console.Write("Mit welchem Wert willst du rechnen?: ");
+                    eingabe1 = Console.ReadLine();
+                    eingabe2 = "h";
 
-                Console.Write("Gib den Operator ein: ");
-                op = Console.ReadLine();
+                    Wert1(eingabe1,eingabe2,wert1,wert2);
 
-                Console.Write("Gib eine zweite Zahlt ein: ");
-                zahl2 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case "h":
 
-                switch (rechnen)
-                {
-                    case 1:
-                        double solution = berechneOperation(zahl1, zahl2, result, op);
+                    break;
+                case "A":
 
-                        Console.WriteLine($"Resultat: {solution}");
-                        break;
-                    case 2:
-                        //double solution1 = weiterrechnen(zahl1, zahl2, result, op);
+                    break;
+                case "U":
 
-                        Console.WriteLine($"Resultat: {solution1}");
-                        break;
-
-                }
-
-
-
-                /*while (y)
-                {
-                    Console.Write("Mit Ergebnis weiterrechnen [y/n]");
-                    string antwort = Console.ReadLine();
-
-                    if (antwort == "y")
-                    {                       
-                        y = false;
-                        rechnen = 2;
-                    }
-                    else if (antwort == "n")
-                    {                      
-                        rechnen = 1;
-                        y = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ungültige Eingabe");
-                    }
-                }*/
-
+                    break;
             }
 
-            //Console.WriteLine($"Ergebnis = {result}");
-
+        
         }
-
-        /*static double weiterrechnen(double zahl1, double zahl2, double result, string op)
+        static void Wert1(string eingabe1, string eingabe2, double wert1, double wert2 )
         {
-
-            if (op == "+")
+            switch (eingabe1)
             {
-                result = result + zahl2;
+                case "r":
+                    Console.Write("r = ");
+                    wert1 = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case "d":
 
-                return result;
-            }
-            else if (op == "-")
-            {
-                result = result - zahl2;
+                    break;
+                case "v":
 
-                return result;
-            }
-            else if (op == "*")
-            {
-                result = result * zahl2;
+                    break;
+                case "h":
 
-                return result;
-            }
-            else //if (op == "/")
-            {
-                result = result / zahl2;
+                    break;
+                case "U":
 
-                return result;
-            }
-        }*/
+                    break;
+                case "A":
 
-        static double berechneOperation(double zahl1, double zahl2, double result, string op)
-        {
+                    break;
+               
 
-            if (op == "+")
-            {
-                result = zahl1 + zahl2;
-
-                return result;
-            }
-            else if (op == "-")
-            {
-                result = zahl1 - zahl2;
-
-                return result;
-            }
-            else if (op == "*")
-            {
-                result = zahl1 * zahl2;
-
-                return result;
-            }
-            else //if (op == "/")
-            {
-                result = zahl1 / zahl2;
-
-                return result;
-            }
-
-
-
-
-
+            }            
         }
+        static double Result(int result, int wert1, int wert2)
+        {
+            Console.Write($"{wert1} = ");
+            wert1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"{wert2} = ");
+            wert2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (result)
+            {
+                case 'r':
+
+                    break;
+                case 'd':
+
+                    break;
+                case 'v':
+
+                    break;
+                case 'h':
+
+                    break;
+                case 'U':
+
+                    break;
+                case 'A':
+
+                    break;
+                
+
+            }
+            return result;
+        }
+        static void Wert2(int wert1, string eingabe2)
+        {
+            switch (wert1)
+            {
+                case 'r':
+
+                    break;
+                case 'd':
+
+                    break;
+                case 'v':
+
+                    break;
+                case 'h':
+
+                    break;
+                case 'U':
+
+                    break;
+                case 'A':
+
+                    break;
 
 
+            }
+        }
     }
 }
+
 
 
--- Taschenrechner.cs
+++ Taschenrechner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.IO;
 
 namespace Taschenrechner
 {
@@ -23,23 +24,32 @@ namespace Taschenrechner
                 {
                     Console.Write("Gib eine Zahl ein: ");
                     zahl1 = Convert.ToDouble(Console.ReadLine());
-
+                    
                     while (middle)
                     {
 
                         Console.Write("Gib den Operator ein: ");
                         op = Console.ReadLine();
 
-                        string[] opArray = { "+", "-", "*", "/", "p", "w" };
+                        string[] opArray = { "+", "-", "*", "/", "p", "w", "Pi" };
 
-                        if (!opArray.Contains(op))
+                        while (true)
                         {
-                            Console.WriteLine("Fehlerhafte Eingabe");
+                            if (!opArray.Contains(op))
+                            {
+                                Console.WriteLine("Fehlerhafte Eingabe");
+                            }
+                            break;
                         }
-                        break;
+
+                        if(op == "Grössen")
+                        {
+
+                        }
+
                         Console.Write("Gib eine zweite Zahlt ein: ");
                         zahl2 = Convert.ToDouble(Console.ReadLine());
-
+                        
                         
                         if(zahl1 == 7)
                         {
@@ -49,6 +59,10 @@ namespace Taschenrechner
                             Console.WriteLine($"x1 = {nightresult[0]}");
                             Console.WriteLine($"x2 = {nightresult[1]}");
                             
+                        }else if(op == "Pi")
+                        {
+                            result = annäherungPi(zahl1);
+                            Console.WriteLine(result);
                         }
                         else
                         {
@@ -170,6 +184,58 @@ namespace Taschenrechner
             {
                 Console.WriteLine($"X{i} = {resultArray[i]}");                
             }
+        }
+        static double annäherungPi(double resultPi)
+        {
+            int vorzeichen = 1;
+            double Pi = 0;
+            int k = 0;
+            string path = @"C:\Users\joshu\source\repos\LA_Lernperiode-2\Test\Zahl_PI.txt";
+            bool stopPi = false;
+
+            Task.Run(() =>
+            {
+                Console.ReadLine();
+                stopPi = true;
+            });
+
+
+            while (true)
+            {
+                Pi += vorzeichen * (1.0 / (2 * k + 1)) * 4;
+                vorzeichen *= -1;
+                k++;
+
+                if (stopPi)
+                {
+                    Console.WriteLine("Programm angehalten.");
+                    break;
+                }
+
+                Console.WriteLine(Pi);
+
+                bool writing = false;
+                int count = 0;
+
+
+                while (!writing && count < 5)
+                {
+                    try
+                    {
+                        File.WriteAllText(path, Pi.ToString());
+                        writing = true;
+                    }
+                    catch (IOException)
+                    {
+                        count++;
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            resultPi = Pi;
+            return resultPi;
+
+        
         }
     }
 }
